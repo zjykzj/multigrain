246504368731081a62ff7bbcf345b93de0c0f274
zjkjzj <wy163zhuj@163.com>
2022-09-06 21:12:59 +0800

    test(model.py): update

---
+++ test/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2022/9/6 下午3:01
+@file: __init__.py.py
+@author: zj
+@description: 
+"""
---
+++ test/model.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2022/9/6 下午3:01
+@file: model.py
+@author: zj
+@description: 
+"""
+
+from multigrain.lib import get_multigrain
+
+if __name__ == '__main__':
+    model = get_multigrain()
+    print(model)
